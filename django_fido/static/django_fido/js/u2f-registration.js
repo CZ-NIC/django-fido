d7600c7a234ed6441e213fedeadae3f19dae4bb9
Maryna <marina.krivosheyenko@gmail.com>
2018-05-16 12:01:29 +0200

    Ticket #21788 - Fix JS

--- django_fido/static/django_fido/js/u2f-registration.js
+++ django_fido/static/django_fido/js/u2f-registration.js
@@ -1,72 +1,97 @@
-const U2F_FORM_ID = 'django-fido-u2f-form'
-const U2F_ERROR_LIST_ID = 'django-fido-u2f-errors'
-// U2F request identifiers - shared between code and JS
-const U2F_REGISTRATION_REQUEST = 'registration'
-const U2F_AUTHENTICATION_REQUEST = 'authentication'
-// Timeout for the U2F request
-const U2F_TIMEOUT = 30
-const U2F_ERROR_CODES = {
-    1: 'An error occurred while processing U2F request',
-    2: 'U2F request cannot be processed.',
-    3: 'Your configuration for U2F is not supported.',
-    4: 'The presented device is not eligible for this request.',
-    5: 'U2F request timed out.'
-}
-
-function addU2fError(message) {
-    const error_list = document.getElementById(U2F_ERROR_LIST_ID);
-    const new_item = document.createElement("li");
-    new_item.classList.add('error');
-    new_item.appendChild(document.createTextNode(gettext(message)));
-    error_list.appendChild(new_item);
-}
-
-function isU2fAvailabile() {
-    return (typeof window.u2f !== 'undefined');
-}
-
-function u2fResponseCallback(u2f_response) {
-    if (u2f_response.errorCode) {
-        addU2fError(U2F_ERROR_CODES[u2f_response.errorCode]);
-        return
+class U2FRegistration {
+    constructor () {
+        this.U2F_FORM_ID = 'django-fido-u2f-form';
+        this.U2F_ERROR_LIST_ID = 'django-fido-u2f-errors';
+        this.U2F_REGISTRATION_REQUEST = 'registration';
+        this.U2F_AUTHENTICATION_REQUEST = 'authentication';
+        this.U2F_TIMEOUT = 5;
+        this.U2F_ERROR_CODES = {
+            1: gettext('An error occurred while processing U2F request'),
+            2: gettext('U2F request cannot be processed.'),
+            3: gettext('Your configuration for U2F is not supported.'),
+            4: gettext('The presented device is not eligible for this request.'),
+            5: gettext('U2F request timed out.')
+        };
+        this.u2fResponseCallback = this.u2fResponseCallback.bind(this);
+        this.startU2f = this.startU2f.bind(this);
     }
-    const form = document.getElementById(U2F_FORM_ID);
-    form.u2f_response.value = JSON.stringify(u2f_response);
-    form.submit();
-}
 
-function processU2fRequest(formDataset) {
-    const http_request = new XMLHttpRequest();
-    http_request.onreadystatechange = () => {
-        if (http_request.readyState === 4 && http_request.status === 200) {
-            const u2f_request = JSON.parse(http_request.responseText);
-            if (formDataset.mode === U2F_AUTHENTICATION_REQUEST) {
-                u2f.sign(u2f_request.appId, u2f_request.challenge, u2f_request.registeredKeys, u2fResponseCallback, U2F_TIMEOUT);
-            } else if (formDataset.mode === U2F_REGISTRATION_REQUEST) {
-                u2f.register(u2f_request.appId, u2f_request.registerRequests, u2f_request.registeredKeys, u2fResponseCallback, U2F_TIMEOUT);
-            } else {
-                addU2fError("Unknown U2F request.");
-                return
-            }
+    checkGettext() {
+        if (typeof(gettext) === "undefined") {
+            gettext = msg => msg;
         }
-    };
-    http_request.open('GET', formDataset.url, true);
-    http_request.send();
+    }
+
+    createErrorListElem() {
+        const form = document.querySelector(`#${this.U2F_FORM_ID} fieldset`);
+        const error_list = document.createElement("ul");
+        error_list.id = this.U2F_ERROR_LIST_ID;
+        form.prepend(error_list);
+        return error_list;
+    }
+
+    addU2fError(message) {
+        let error_list = document.getElementById(this.U2F_ERROR_LIST_ID) ? document.getElementById(this.U2F_ERROR_LIST_ID) : this.createErrorListElem();
+        const new_item = document.createElement("li");
+        new_item.classList.add('error');
+        new_item.appendChild(document.createTextNode(message));
+        error_list.appendChild(new_item);
+    }
+
+    isU2fAvailabile() {
+        return (typeof window.u2f !== 'undefined');
+    }
+
+    u2fResponseCallback(u2f_response){
+        if (u2f_response.errorCode) {
+            this.addU2fError(this.U2F_ERROR_CODES[u2f_response.errorCode]);
+            return
+        }
+        const form = document.getElementById(this.U2F_FORM_ID);
+        form.u2f_response.value = JSON.stringify(u2f_response);
+        form.submit();
+    }
+
+    processU2fRequest(formDataset) {
+        fetch(formDataset.url, {
+            method: 'get',
+            credentials: 'same-origin'
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.text();
+                }
+            })
+            .then((text) => {
+                const u2f_request = JSON.parse(text);
+                if (formDataset.mode === this.U2F_AUTHENTICATION_REQUEST) {
+                    u2f.sign(u2f_request.appId, u2f_request.challenge, u2f_request.registeredKeys, this.u2fResponseCallback, this.U2F_TIMEOUT);
+                } else if (formDataset.mode === this.U2F_REGISTRATION_REQUEST) {
+                    u2f.register(u2f_request.appId, u2f_request.registerRequests, u2f_request.registeredKeys, this.u2fResponseCallback, this.U2F_TIMEOUT);
+                } else {
+                    this.addU2fError(gettext("Unknown U2F request."));
+                    return;
+                }
+            });
+    }
+    
+    startU2f(){
+        this.checkGettext();
+        const form = document.getElementById(this.U2F_FORM_ID);
+        if (!form) {
+            // Silently skip if not on correct page.
+            return
+        }
+        if (!this.isU2fAvailabile()) {
+            this.addU2fError(gettext("U2F is not available"));
+            return
+        }
+        if (form.dataset.mode) {
+            this.processU2fRequest(form.dataset);
+        }
+    }
+
 }
 
-function startU2f() {
-    const form = document.getElementById(U2F_FORM_ID);
-    if (!form) {
-        // Silently skip if not on correct page.
-        return
-    }
-    if (!isU2fAvailabile()) {
-        addU2fError("U2F is not available");
-        return
-    }
-    if (form.dataset.mode) {
-        processU2fRequest(form.dataset);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', startU2f, false);
+let reg = new U2FRegistration()
+document.addEventListener('DOMContentLoaded', reg.startU2f, false);
