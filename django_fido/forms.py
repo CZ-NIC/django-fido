50b029e65a2b131b2d1f01a1de47e88380cc8058
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-04-30 14:51:05 +0200

    Add FIDO 2 registration form

--- django_fido/forms.py
+++ django_fido/forms.py
@@ -1,11 +1,44 @@
 """Forms for U2F registration and login."""
 from __future__ import unicode_literals
 
+import base64
 import json
 
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from fido2.client import ClientData
+from fido2.ctap2 import AttestationObject
+
+
+class Fido2RegistrationForm(forms.Form):
+    """Form for FIDO 2 registration responses."""
+
+    client_data = forms.CharField(error_messages={'required': _("Operation wasn't completed.")},
+                                  widget=forms.HiddenInput)
+    attestation = forms.CharField(error_messages={'required': _("Operation wasn't completed.")},
+                                  widget=forms.HiddenInput)
+
+    class Media:
+        """Add FIDO 2 related JS."""
+
+        js = ('django_fido/js/fido2.js', )
+
+    def clean_client_data(self) -> ClientData:
+        """Return decoded client data."""
+        value = self.cleaned_data['client_data']
+        try:
+            return ClientData(base64.b64decode(value))
+        except ValueError:
+            raise ValidationError(_('FIDO 2 response is malformed.'), code='invalid')
+
+    def clean_attestation(self) -> AttestationObject:
+        """Return decoded attestation object."""
+        value = self.cleaned_data['attestation']
+        try:
+            return AttestationObject(base64.b64decode(value))
+        except ValueError:
+            raise ValidationError(_('FIDO 2 response is malformed.'), code='invalid')
 
 
 class U2fResponseForm(forms.Form):
--- django_fido/tests/test_forms.py
+++ django_fido/tests/test_forms.py
@@ -1,9 +1,51 @@
 """Test `django_fido.forms` module."""
 from __future__ import unicode_literals
 
-from django.test import SimpleTestCase
+import base64
 
-from django_fido.forms import U2fResponseForm
+from django.test import SimpleTestCase
+from fido2.client import ClientData
+from fido2.ctap2 import AttestationObject
+
+from django_fido.forms import Fido2RegistrationForm, U2fResponseForm
+
+from .data import ATTESTATION_OBJECT
+
+
+class TestFido2RegistrationForm(SimpleTestCase):
+    def test_valid(self):
+        # Test form with valid client data and attestation
+        form = Fido2RegistrationForm({'client_data': 'eyJjaGFsbGVuZ2UiOiAiR2F6cGFjaG8hIn0=',
+                                      'attestation': ATTESTATION_OBJECT})
+
+        self.assertTrue(form.is_valid())
+        cleaned_data = {'client_data': ClientData(b'{"challenge": "Gazpacho!"}'),
+                        'attestation': AttestationObject(base64.b64decode(ATTESTATION_OBJECT))}
+        self.assertEqual(form.cleaned_data, cleaned_data)
+
+    def test_clean_client_data_empty(self):
+        form = Fido2RegistrationForm({'client_data': '', 'attestation': ATTESTATION_OBJECT})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'client_data': ["Operation wasn't completed."]})
+
+    def test_clean_client_data_invalid(self):
+        form = Fido2RegistrationForm({'client_data': 'A', 'attestation': ATTESTATION_OBJECT})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'client_data': ['FIDO 2 response is malformed.']})
+
+    def test_clean_attestation_empty(self):
+        form = Fido2RegistrationForm({'client_data': 'e30=', 'attestation': ''})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'attestation': ["Operation wasn't completed."]})
+
+    def test_clean_attestation_invalid(self):
+        form = Fido2RegistrationForm({'client_data': 'e30=', 'attestation': 'A'})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'attestation': ['FIDO 2 response is malformed.']})
 
 
 class TestU2fResponseForm(SimpleTestCase):
