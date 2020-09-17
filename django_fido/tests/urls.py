24dd3af082266137359fb9fa8305676a08a865af
tpazderka <tomas.pazderka@nic.cz>
2020-09-17 14:54:29 +0200

    Form error on unsupported attestation format (#94)
    * Form error on unsupported attestation format

--- django_fido/tests/urls.py
+++ django_fido/tests/urls.py
@@ -2,7 +2,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django_fido.views import AttestationConveyancePreference, Fido2RegistrationView
+
 urlpatterns = [
     url('', include('django_fido.urls')),
     url('admin/', admin.site.urls),
+    url('direct_registration/',
+        Fido2RegistrationView.as_view(attestation=AttestationConveyancePreference.DIRECT),
+        name='registration_direct'),
 ]
