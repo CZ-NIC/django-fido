d83779717ae780172c2b89f34ce2d6509e68c29d
James Lin <james.lin@btg.co.nz>
2021-11-15 09:14:08 +1300

    refactored backends to reduce duplicate code, added info in readme

--- django_fido/backends.py
+++ django_fido/backends.py
@@ -26,14 +26,39 @@ def is_fido_backend_used() -> bool:
     return False
 
 
-class Fido2AuthenticationBackend(object):
+class Fido2AuthenticationBackendMixin(object):
     """
-    Authenticate user using FIDO 2.
+    Base class for authenticating user using FIDO 2.
 
     @cvar counter_error_message: Error message in case FIDO 2 device counter didn't increase.
     """
 
     counter_error_message = _("Counter of the FIDO 2 device decreased. Device may have been duplicated.")
+
+    def mark_device_used(self, device, counter):
+        """Update FIDO 2 device usage information."""
+        if counter == 0 and device.counter == 0:
+            # Counter is unsupported by the device, bail out early
+            return
+        if counter <= device.counter:
+            _LOGGER.info("FIDO 2 authentication failed because of not increasing counter.")
+            raise ValueError("Counter didn't increase.")
+        device.counter = counter
+        device.full_clean()
+        device.save()
+
+    def get_user(self, user_id):
+        """Return user based on its ID."""
+        try:
+            return get_user_model().objects.get(pk=user_id)
+        except get_user_model().DoesNotExist:
+            return None
+
+
+<<<<<<< HEAD
+=======
+class Fido2AuthenticationBackend(Fido2AuthenticationBackendMixin):
+    """Authenticate user using FIDO 2."""
 
     def authenticate(self, request: HttpRequest, user: AbstractBaseUser, fido2_server: Fido2Server,
                      fido2_state: Dict[str, bytes], fido2_response: Dict[str, Any]) -> Optional[AbstractBaseUser]:
@@ -56,26 +81,39 @@ class Fido2AuthenticationBackend(object):
             raise PermissionDenied("Counter didn't increase.")
         return user
 
-    def mark_device_used(self, device, counter):
-        """Update FIDO 2 device usage information."""
-        if counter == 0 and device.counter == 0:
-            # Counter is unsupported by the device, bail out early
-            return
-        if counter <= device.counter:
-            _LOGGER.info("FIDO 2 authentication failed because of not increasing counter.")
-            raise ValueError("Counter didn't increase.")
-        device.counter = counter
-        device.full_clean()
-        device.save()
 
-    def get_user(self, user_id):
-        """Return user based on its ID."""
+class Fido2PasswordlessAuthenticationBackend(Fido2AuthenticationBackendMixin):
+    """Authenticate user using FIDO 2 passwordlessly using supplied user handle."""
+
+    def authenticate(self, request: HttpRequest, fido2_server: Fido2Server,
+                     fido2_state: Dict[str, bytes], fido2_response: Dict[str, Any]) -> Optional[AbstractBaseUser]:
+        """Authenticate using FIDO 2."""
+        user_handle = base64.b64encode(fido2_response['user_handle']).decode('utf-8')
         try:
-            return get_user_model().objects.get(pk=user_id)
-        except get_user_model().DoesNotExist:
+            authenticator = Authenticator.objects.get(user_handle=user_handle)
+            user = authenticator.user
+            credentials = [authenticator.credential]
+            credential = fido2_server.authenticate_complete(
+                fido2_state, credentials, fido2_response['credential_id'], fido2_response['client_data'],
+                fido2_response['authenticator_data'], fido2_response['signature'])
+        except ValueError as error:
+            _LOGGER.info("FIDO 2 authentication failed with error: %r", error)
+            return None
+        except Authenticator.DoesNotExist:
+            _LOGGER.info("FIDO 2 authentication could not find user handle: %s", user_handle)
             return None
 
+        device = user.authenticators.get(credential_id_data=base64.b64encode(credential.credential_id).decode('utf-8'))
+        try:
+            self.mark_device_used(device, fido2_response['authenticator_data'].counter)
+        except ValueError:
+            # Raise `PermissionDenied` to stop the authentication process and skip remaining backends.
+            messages.error(request, self.counter_error_message)
+            raise PermissionDenied("Counter didn't increase.")
+        return user
 
+
+>>>>>>> d42b0bb (refactored backends to reduce duplicate code, added info in readme)
 class Fido2GeneralAuthenticationBackend(ModelBackend):
     """Authenticated user using any username-password backend and Fido2AuthenticationBackend."""
 
