72529e34b9b05c8f47f0e4f6fb2e3174efddfa16
Přemysl Pazderka <premysl.pazderka@nic.cz>
2025-09-23 19:03:41 +0200

    Upgrade NPM dependencies

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+const { defineConfig } = require('eslint/config')
+
+module.exports = defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                gettext: true,
+            },
+        },
+    },
+    {
+        files: ['**/*.test.js'],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+                ...globals.node,
+            }
+        }
+    }
+]);
